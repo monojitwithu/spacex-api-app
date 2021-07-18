5168cfb867965c3fabc53e618a3b70e48c4efaaa
monojitwithu <monojitwithu@gmail.com>
2021-07-18 22:11:53 +0530

    container Created first card compleated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  class App extends Component{
+   
+  constructor(){
+    super()
+    this.state={
+      rockets:[]
+
+    }
+
+    
+
+
+  }
+  componentDidMount(){
+    fetch("https://api.spacexdata.com/v4/rockets")
+    .then((buffer)=>buffer.json())
+    .then((response)=>this.setState({rockets:response}))
+    
+  }
+  
+  render(){
+    console.log(this.state.rockets)
+    return(
+      <div className="container" >
+        <div className="row">
+      {this.state.rockets.map((item)=>(
+      
+      <div className="card" style={{width: "18rem"}} key={item.id}>
+      <img src="..." className="card-img-top" alt="..."/>
+     <div className="card-body">
+    <h5 className="card-title">{item.name}</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+    </div>)   )}
+
     </div>
-  );
+     
+
+      
+ 
+
+
+
+
+    
+
+
+
+      </div>
+    )
+  }
+
+
+
 }
 
 export default App;
